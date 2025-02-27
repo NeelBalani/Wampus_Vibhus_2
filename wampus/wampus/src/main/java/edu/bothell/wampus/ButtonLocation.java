60b39e0612a70172c4b652afcf666ec8a9949b68
Neel B <2010725@apps.nsd.org>
2025-02-27 14:09:02 -0800

    BtnLocation

--- wampus/wampus/src/main/java/edu/bothell/wampus/ButtonLocation.java
+++ wampus/wampus/src/main/java/edu/bothell/wampus/ButtonLocation.java
@@ -12,8 +12,38 @@ import javax.swing.JOptionPane;
 
 public class ButtonLocation implements Locatable, ActionListener{
 
+    private int x;
+    private int y;
+    private GameLocation location;
+
     public ButtonLocation(){
         
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        
+    }
+
+    @Override
+    public int getX(){
+        return x;
+    }
+
+    @Override
+    public int getY(){
+        return y;
+    }
+
+    @Override
+    public int[] getPos(){
+        int[] array = new int[] {x,y};
+        return array;
+    }
+
+    @Override
+    public GameLocation getLocation(){
+        return location;
     }
     
 }
