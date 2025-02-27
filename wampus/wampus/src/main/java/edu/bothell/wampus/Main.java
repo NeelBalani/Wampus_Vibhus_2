264999eec4c4207dbafeb6c12ab0e95785196956
Neel B <2010725@apps.nsd.org>
2025-02-27 14:52:13 -0800

    buttons creation

--- wampus/wampus/src/main/java/edu/bothell/wampus/Main.java
+++ wampus/wampus/src/main/java/edu/bothell/wampus/Main.java
@@ -2,11 +2,12 @@ package edu.bothell.wampus;
 
 public class Main {
     public static void main(String[] args) {
-        Controller controller = new Controller();
+        new GUI();
+        /*Controller controller = new Controller();
         UI ui = new ConsoleUI(controller);
 
         controller.addPerson(new Pranav("Pranav"));
 
-        //controller.start();
+        //controller.start();*/
     }
 }
