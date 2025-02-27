d70b667fcc292f7e143bab6200151c6dbfc91b54
Neel B <2010725@apps.nsd.org>
2025-02-27 14:34:03 -0800

    JFrame up

--- wampus/wampus/src/main/java/edu/bothell/wampus/GUI.java
+++ wampus/wampus/src/main/java/edu/bothell/wampus/GUI.java
@@ -1,5 +1,22 @@
 package edu.bothell.wampus;
 
+import java.awt.Button;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 public class GUI {
+
+    public GUI(){
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+    }
+
+    
        
 }
