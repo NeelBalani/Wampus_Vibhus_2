8709e0df117922bc957a3ca08cfa1db95c30531f
pranavmullakkal <1108132@apps.nsd.org>
2025-02-27 14:39:14 -0800

    GUI imports pranav

--- wampus/wampus/src/main/java/edu/bothell/wampus/GUI.java
+++ wampus/wampus/src/main/java/edu/bothell/wampus/GUI.java
@@ -1,14 +1,9 @@
 package edu.bothell.wampus;
 
-import java.awt.Button;
-import java.awt.Dimension;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 public class GUI {
 
