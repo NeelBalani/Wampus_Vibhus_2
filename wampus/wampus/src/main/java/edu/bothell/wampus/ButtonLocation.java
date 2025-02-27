8e89ab520db91377a800d8e790b774d0e2a0c3db
Neel B <2010725@apps.nsd.org>
2025-02-27 14:02:15 -0800

    ButtonLocation

---
+++ wampus/wampus/src/main/java/edu/bothell/wampus/ButtonLocation.java
@@ -0,0 +1,19 @@
+package edu.bothell.wampus;
+
+import java.awt.Button;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class ButtonLocation implements Locatable, ActionListener{
+
+    public ButtonLocation(){
+        
+    }
+    
+}
