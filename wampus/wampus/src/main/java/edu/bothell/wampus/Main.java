1277d856d1385d4710f9e33bf1547d46e70fc57f
pranavmullakkal <1108132@apps.nsd.org>
2025-03-14 15:10:44 -0700

    File reading tried out FAILURE

--- wampus/wampus/src/main/java/edu/bothell/wampus/Main.java
+++ wampus/wampus/src/main/java/edu/bothell/wampus/Main.java
@@ -2,16 +2,30 @@ package edu.bothell.wampus;
 
 public class Main {
     public static void main(String[] args) {
-        // Initialize the controller and UI
         Controller controller = new Controller();
         UI ui = new ConsoleUI(controller);
         controller.addPerson(new Pranav("Pranav"));
         controller.addPlayersToLocationManager();
         new GUI(controller);
         controller.setNewPlayers();
-        System.out.println(
-            "is it testing"
-        );
+        System.out.println("Current working directory: " + System.getProperty("user.dir"));
+
+        
+        QuestionInitializer questionInitializer = new QuestionInitializer();
+        TriviaManager triviaManager = questionInitializer.fromFile("trivia.csv");
+        
+        for (int i = 0; i < triviaManager.getTotalQuestions(); i++) {
+            System.out.println("Question " + (i + 1) + ": " + triviaManager.getQuestion(i));
+            System.out.println("Correct Answer: " + triviaManager.getCorrectAnswer(i));
+            System.out.print("Possible Answers: ");
+            for (String answer : triviaManager.getPossibleAnswers(i)) {
+                System.out.print(answer + " ");
+            }
+            System.out.println("\n");
+        }
+
+        
+        System.out.println("is it testing");
         // controller.start();
     }
 }
