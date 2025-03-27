4942a80d7b6aa5985a3e0c7c32d471da0f18affb
Vibhu Rampally <2011243@apps.nsd.org>
2025-03-27 15:15:44 -0700

    sdf

--- wampus/wampus/src/main/java/edu/bothell/wampus/WampusController.java
+++ wampus/wampus/src/main/java/edu/bothell/wampus/WampusController.java
@@ -1,16 +1,34 @@
 package edu.bothell.wampus;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/api")
 public class WampusController{
 
+    private Cave cave = new Cave();
+    private List<Person> players = new ArrayList<Person>();
+    private Game game = new Game(new LocationManager(this.cave), null);
+
+
     @GetMapping("/hello")
     public String hello() {
         return "tapushity";
     }
+
+    @GetMapping("/location/{i}")
+    public String getCurrentRoom(@PathVariable("i") String i) {
+        GameLocation gl = (new GameLocation(0,1,5));
+        return " " + i;
+    }    
+    
 }
 
