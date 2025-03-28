94c17fd68c4c16d66777791dedab22904587c76b
pranavmullakkal <1108132@apps.nsd.org>
2025-03-28 14:41:04 -0700

    web gui start pranav

--- wampus/wampus/src/main/java/edu/bothell/wampus/WampusController.java
+++ wampus/wampus/src/main/java/edu/bothell/wampus/WampusController.java
@@ -1,16 +1,15 @@
 package edu.bothell.wampus;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/api")
 public class WampusController{
-
     @GetMapping("/hello")
     public String hello() {
-        return "tapushity";
+        return "sigma sigma boy sigma boy sigma boy";
     }
 }
 
