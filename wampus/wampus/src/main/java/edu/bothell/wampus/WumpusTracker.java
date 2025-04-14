3fb45d4ea1608e8bd8fdec467d112f0c98b5043f
Vibhu Rampally <2011243@apps.nsd.org>
2025-04-14 15:14:25 -0700

    finished the assignment

--- wampus/wampus/src/main/java/edu/bothell/wampus/WumpusTracker.java
+++ wampus/wampus/src/main/java/edu/bothell/wampus/WumpusTracker.java
@@ -7,17 +7,17 @@ public class WumpusTracker {
     private ArrayList<Integer> visitedRooms;
 
     public WumpusTracker(Room[] rooms, ArrayList<Integer> visits) {
-        roomArray = rooms;
-        visitedRooms = visits;
+        this.roomArray = rooms;
+        this.visitedRooms = visits;
     }
 
     /** Returns the average danger level of all visited rooms */
     public double getAverageDanger() {
         double average = 0;
-        for(int n = 0; n < visitedRooms.size(); n++){
-            average += visitedRooms.get(n);
+        for(int roomId : this.visitedRooms){
+            average += getRoomById(roomId).getDangerLevel();
         }
-        average /= visitedRooms.size();
+        average /= this.visitedRooms.size();
         return average;
     }
 
@@ -25,11 +25,23 @@ public class WumpusTracker {
     public int getMostDangerousVisitedRoom() {
         int room = 0;
         int dangerLevel = 0;
-        for(int n = 0; n < visitedRooms.size(); n++){
-            if(dangerLevel < visitedRooms.get(n)){
-                dangerLevel = n;
+        for(int roomId : this.visitedRooms){
+            if(getRoomById(roomId).getDangerLevel() > dangerLevel){
+                room = roomId;
+                dangerLevel = getRoomById(roomId).getDangerLevel();
             }
         }
         return room;
+    }
+
+    public void addVisitedRoom(int roomId) {
+        this.visitedRooms.add(roomId);
+    }
+
+    public Room getRoomById(int roomId){
+        for(Room room : this.roomArray){
+            if(room.getID() == roomId) return room;
+        }
+        return null;
     }
 }
