787f59e8eadfa19d79651e4234a1baab26ff73d6
NeelBalani <2010725@apps.nsd.org>
2025-05-26 20:49:36 -0700

    Obstacle got one more method and pits and wumpus don't got it yet, bats might work idk test it.

--- wampus/wampus/src/main/java/edu/bothell/wampus/interfaces/Obstacle.java
+++ wampus/wampus/src/main/java/edu/bothell/wampus/interfaces/Obstacle.java
@@ -1,5 +1,7 @@
 package edu.bothell.wampus.interfaces;
 
+import edu.bothell.wampus.Game;
+import edu.bothell.wampus.controllers.GameController;
 import edu.bothell.wampus.models.AdjacentGameLocation;
 
 public interface Obstacle {
@@ -10,6 +12,7 @@ public interface Obstacle {
     void triggerObstacle();
     void destroyObstacle();
     boolean isObstacleDestroyed();
+    void doAction(GameController controller);
     AdjacentGameLocation getLocation();
     String toString();
     String getWarning();
